ce7bab7d73b851fa72d093d4ec2b428b3f1247e4
Mazen Magdy <mazenmagdy100.mm@gmail.com>
2021-11-26 17:40:27 +0200

    adding our girl Ylva to the dict

--- src/app.py
+++ src/app.py
@@ -3,12 +3,20 @@
 
 
 animedict = {
+    
+    "thorfinn" : {
     "Name" : "Thorfinn" ,
     "anime" : "Vinland Saga" ,
     "Gender" : "Male" ,
     "Weapon" : "Two daggers"
+    } ,
+    "Ylva" : {
+    "Name" : "Ylva" ,
+    "anime" : "Vinland Saga" ,
+    "Gender" : "Female" ,
+    "Weapon" : "A woman ha"
+    } 
 }
-
 for x in animedict :
     print(animedict[x])
 
