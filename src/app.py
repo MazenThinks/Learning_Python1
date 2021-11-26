bcf2c477d26779d6bdd99a93e73aa192d08cf7d4
Mazen Magdy <mazenmagdy100.mm@gmail.com>
2021-11-26 16:54:46 +0200

    dublicating 2 sets

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 
 
-anime_set = set(("Levi" , "Torphin" , "luther"))
+anime_set = set(("Levi" , "Torphin" , "Luther"))
 
+anime_set2 = {"Ereh" , "Ylva" , "Helga"}
 
-anime_set.add("Takemichi")
+anime_set.update(anime_set2)
 
 print(anime_set)
 
