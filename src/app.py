931c0d1b33c58beb5c9342a55cba04eb2784020d
Mazen Magdy <mazenmagdy100.mm@gmail.com>
2021-11-26 16:43:41 +0200

    Learning sets boii

--- src/app.py
+++ src/app.py
@@ -1,23 +1,56 @@
-print("Hello world !")
-
-# Kg to Lb
-
-kg = input("Enter your weight(Kg) : ")
-
-lb = float(kg) * 2.20462262
-
-print("the weight(lb) : " + str(lb))
-
-# Circle's area
-
-radius = float(input("Enter the circle's radius(cm) : "))
-
-area = 22/7 * radius**2
-
-print("Circle's area = " + str(area) + "cm2")
 
 
- 
+anime_set = set(("Levi" , "Torphin" , "luther"))
+
+print(type(anime_set))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
