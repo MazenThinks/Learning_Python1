7072c70db5aa90ee3a5438b82052852d2870c9e4
Mazen Magdy <mazenmagdy100.mm@gmail.com>
2021-11-27 14:09:34 +0200

    The introduction of while loops ( Infinite loop )

--- src/app.py
+++ src/app.py
@@ -1,27 +1,44 @@
 
-#Dictionaries
+
+# The introduction of while loops :)
+
+# infinte loop
 
 
-animedict = {
-    
-    "thorfinn" : {
-    "Name" : "Thorfinn" ,
-    "anime" : "Vinland Saga" ,
-    "Gender" : "Male" ,
-    "Weapon" : "Two daggers"
-    } ,
-    "Ylva" : {
-    "Name" : "Ylva" ,
-    "anime" : "Vinland Saga" ,
-    "Gender" : "Female" ,
-    "Weapon" : "A woman ha"
-    } 
-}
-for x in animedict :
-    print(animedict[x])
+x = 1
+
+while x < 2 :
+    print(x)
 
 
-# aaand Dictionaries end hea :D
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
