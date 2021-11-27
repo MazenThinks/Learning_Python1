aa48f90803897a06f305814662e02224ec1f2e6c
Mazen Magdy <mazenmagdy100.mm@gmail.com>
2021-11-27 14:23:54 +0200

    controlled loop 2

--- src/app.py
+++ src/app.py
@@ -2,16 +2,16 @@
 
 # The introduction of while loops :)
 
-# infinte loop
-
-
-x = 1
+x = 1                                 
 
 while x < 10 :
+    x += 1
+    if x == 10 :
+        continue
     print(x)
-    x = x + 1
 
 
+   
 
 
 
