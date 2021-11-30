13ce0dc8fcd8a591112142b9c248853f34372402
Mazen Magdy <mazenmagdy100.mm@gmail.com>
2021-11-30 22:30:13 +0200

    classes

--- src/app.py
+++ src/app.py
@@ -1,9 +1,4 @@
-n = input("gonna double ur numba just type it : ")
+class MyClass :
+    x = 1
 
-def my_function():
-    global n
-    return lambda a : a * n
-
-function_doubler = my_function()
-
-print(function_doubler(2))
+print(MyClass.x)
